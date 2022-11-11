365c55d4de453acc945d0a29707180dc0d8dca66
song-tiigger <applej07@naver.com>
2022-11-11 17:40:40 +0900

    11/11 5:40 송하정

---
+++ Visit/Culture_Space/js/culture_space.js
@@ -0,0 +1,47 @@
+$(function() {
+    $('.tab li').click(function (e) {
+        e.preventDefault();
+        let idx = $(this).index();
+
+        $('.space-header').removeClass('show');
+        $('.space-header').eq(idx).addClass('show');
+
+        $('section').removeClass('show');
+        $('section').eq(idx).addClass('show');
+    });
+
+
+
+    // var scroll_top;
+    // // 인트로 타이틀 위치 조정
+    // function checkIntroTitle() {
+    //     if($('.sodam-intro').outerHeight() < window.innerHeight) {
+    //         $('.sodam-intro').addClass('fixed');
+    //     } else {
+    //         $('.sodam-intro').removeClass('fixed');
+    //     }
+    // }
+
+    $(window).on("scroll", function() {
+        
+        var h = $(window).scrollTop();
+        console.log(h);
+
+        if(h >= 1100) {
+            $('.sodam-intro').addClass('fixed');
+
+        } else {
+            $('.sodam-intro').removeClass('fixed');
+
+        }
+
+    })
+
+
+
+
+
+
+
+});
+
