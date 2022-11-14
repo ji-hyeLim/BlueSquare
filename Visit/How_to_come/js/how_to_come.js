ff26f294ef44c98973d2f0da74728bda3471abd5
SeonahNoh <trsdlo1080@gmail.com>
2022-11-14 19:15:52 +0900

    오시는길 2차수정

--- Visit/How_to_come/js/how_to_come.js
+++ Visit/How_to_come/js/how_to_come.js
@@ -4,5 +4,8 @@ $(function() {
         let idx = $(this).index();
         $('section').removeClass('show');
         $('section').eq(idx).addClass('show');
+
+        $('.tap-menu li a').removeClass('on');
+        $('.tap-menu li a').eq(idx).addClass('on');
     });
 });
