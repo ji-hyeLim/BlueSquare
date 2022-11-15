b1be1de4188446a87d13272b48084e2e6e388971
song-tiigger <applej07@naver.com>
2022-11-15 09:20:46 +0900

    11/14 송하정

---
+++ Venue/Venue_Guidance/sub/nemo/js/nemo.js
@@ -0,0 +1,53 @@
+$(function () {
+  $('.sub-tab li').click(function(e) {
+    e.preventDefault();
+    let idx = $(this).index();
+
+    $('section > div').removeClass('active');
+    $('section > div').eq(idx).addClass('active');
+
+    $('.sub-tab li a').removeClass('sub-tab-clicked');
+    $('.sub-tab li a').eq(idx).addClass('sub-tab-clicked');
+  })
+
+  // ======================== 이미지 넘기기 마스터카드홀 ========================
+  $('.prev').click(function (e) {
+    e.preventDefault();
+    let imgOn = $('#master .slide-img').find('.on').index(); // 현재 이미지 위치
+    let imgLen = $('#master .slide-img .item').length;
+    // console.log('이미지 위치:', imgOn);
+    // console.log('이미지 개수:', imgLen);
+
+    $("#master .slide-img .item").eq(imgOn).removeClass("on");
+    $("#master .slide-img .item").eq(imgOn).css("opacity", 0);
+
+    imgOn = imgOn - 1;
+
+    if (imgOn < 0) {
+      $("#master .slide-img .item").eq(imgLen - 1).css("opacity", 1);
+      $("#master .slide-img .item").eq(imgLen - 1).addClass("on");
+    } else {
+      $("#master .slide-img .item").eq(imgOn).css("opacity", 1);
+      $("#master .slide-img .item").eq(imgOn).addClass("on");
+    }
+  });
+
+  $('.next').click(function (e) {
+    e.preventDefault();
+    let imgOn = $('#master .slide-img').find('.on').index(); // 현재 이미지 위치
+    let imgLen = $('#master .slide-img .item').length; // 4
+
+    $("#master .slide-img .item").eq(imgOn).removeClass("on");
+    $("#master .slide-img .item").eq(imgOn).css("opacity", 0);
+
+    imgOn = imgOn + 1;
+
+    if (imgOn === imgLen) {
+      $("#master .slide-img .item").eq(0).css("opacity", 1);
+      $("#master .slide-img .item").eq(0).addClass("on");
+    } else {
+      $("#master .slide-img .item").eq(imgOn).css("opacity", 1);
+      $("#master .slide-img .item").eq(imgOn).addClass("on");
+    }
+  });
+});
--- Visit/Culture_Space/js/culture_space.js
+++ Visit/Culture_Space/js/culture_space.js
@@ -1,15 +1,15 @@
-$(function() {
-    $('.tab li').click(function (e) {
-        e.preventDefault();
-        let idx = $(this).index();
+// $(function() {
+//     $('.tab li').click(function (e) {
+//         e.preventDefault();
+//         let idx = $(this).index();
 
-        $('.space-header').removeClass('show');
-        $('.space-header').eq(idx).addClass('show');
+//         $('.space-header').removeClass('show');
+//         $('.space-header').eq(idx).addClass('show');
 
-        $('section').removeClass('show');
-        $('section').eq(idx).addClass('show');
+//         $('section').removeClass('show');
+//         $('section').eq(idx).addClass('show');
 
-        $('.tab li a').removeClass('clicked');
-        $('.tab li a').eq(idx).addClass('clicked');
-    });
-});
+//         $('.tab li a').removeClass('clicked');
+//         $('.tab li a').eq(idx).addClass('clicked');
+//     });
+// });
