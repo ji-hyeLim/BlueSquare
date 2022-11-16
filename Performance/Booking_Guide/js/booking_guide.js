a62de98944a041be3a96da77cca09ce767a63c19
SeonahNoh <trsdlo1080@gmail.com>
2022-11-16 19:40:37 +0900

    11.16 예매안내 수정중

--- Performance/Booking_Guide/js/booking_guide.js
+++ Performance/Booking_Guide/js/booking_guide.js
@@ -2,5 +2,11 @@ $(document).ready(function () {
     AOS.init ({
         duration : 1000,
         easing : 'linear',
+        once: true
     });
+
+    var hdtxtWrap = $('.hdtxt-wrap');
+    setTimeout(function() {
+        hdtxtWrap.removeClass('content-hidden');
+    }, 3000);
 });
